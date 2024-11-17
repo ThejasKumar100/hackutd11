95c1a7896f7363625f133672bdf7aecd2cfd4489
Thejaswin <thejaskumar100@gmail.com>
2024-11-17 11:22:32 -0600

    Example Tile spawn

--- FE/webApp/src/routes/admin-dashboard/+page.svelte
+++ FE/webApp/src/routes/admin-dashboard/+page.svelte
@@ -72,7 +72,15 @@
                             {/each}
                          {:else}
                          {/if} -->
-                        <p class="no-applications">No pending applications</p>
+
+                        <!-- <p class="no-applications">No pending applications</p> -->
+
+
+                        <div class="card">
+                            <div class="card2">
+                            </div>
+                        </div>
+
                     </div>
                 </section>
             </div>
@@ -251,4 +259,32 @@
             grid-template-columns: 1fr;
         }
     }
+
+    /* ---------------------- Application Tiles */
+    .card {
+        width: 190px;
+        height: 254px;
+        background-image: linear-gradient(163deg, #00ff75 0%, #3700ff 100%);
+        border-radius: 20px;
+        transition: all .3s;
+    }
+
+    .card2 {
+        width: 190px;
+        height: 254px;
+        background-color: #1a1a1a;
+        border-radius: 15px;
+        transition: all .2s;
+    }
+
+    .card2:hover {
+        transform: scale(0.98);
+        border-radius: 20px;
+    }
+
+    .card:hover {
+        box-shadow: 0px 0px 30px 1px rgba(0, 255, 117, 0.30);
+    }
+
+
 </style>
